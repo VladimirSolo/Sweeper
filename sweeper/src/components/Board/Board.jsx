a2d51a52f72b7af2a285a022f2d94bab64886178
Vladimir Solo <freeman.wushu@mail.ru>
2023-03-02 20:22:55 +0600

    create counter opening box

--- sweeper/src/components/Board/Board.jsx
+++ sweeper/src/components/Board/Board.jsx
@@ -6,41 +6,52 @@ import style from './Board.module.css';
 
 export default function Board() {
   const [grid, setGrid] = useState([]);
+  const [bombsCount, setBombsCount] = useState(0);
+  const [bombsPosition, setBombsPosition] = useState([]);
 
   useEffect(() => {
     const newBoard = () => {
       const boardLoad =  createBoard(16, 16, 32);
-      setGrid(boardLoad.board)
+      setBombsCount(16 * 16 - 32);
+      setBombsPosition(boardLoad.bombsPosition);
+      setGrid(boardLoad.board);
     }
-    newBoard()
-  }, [])
+    newBoard();
+  }, []);
 
   const updateFlag = (event, x, y) => {
     event.preventDefault();
     let newGrid = JSON.parse(JSON.stringify(grid));
     newGrid[x][y].flagged = true;
-    setGrid(newGrid)
+    setGrid(newGrid);
   }
 
   const checkCube = (x, y) => {
+    if (grid[x][y].revealed) {
+      return;
+    }
     let newGrid = JSON.parse(JSON.stringify(grid));
     if(newGrid[x][y].value === 'X') {
       alert('bomb')
+      for(let i = 0; i < bombsPosition.length; i++) {
+        newGrid[bombsPosition[i][0]][bombsPosition[i][1]].revealed = true;
+      }
+      setGrid(newGrid);
     } else {
-      let checkedBoard = checked(newGrid, x, y);
-      setGrid(checkedBoard.arr)
+      let checkedBoard = checked(newGrid, x, y, bombsCount);
+      setGrid(checkedBoard.arr);
+      setBombsCount(checkedBoard.bombs);
     }
-    // newGrid[x][y].revealed = true;
-    // setGrid(newGrid);
   }
 
   return (
     <div>
+      <p>{bombsCount}</p>
+      <div className={style.container}>
       {grid ? 
         grid.map((el) => (
-        <div className={style.container} key={Math.random() * 9999}>
+        <div className={style.box} key={Math.random() * 9999}>
           {el.map((item) => (
-
             <Cube cell={item} checkCube={checkCube} updateFlag={updateFlag} key={Math.random() * 9999} />
           ) )}
           </div>
@@ -48,6 +59,7 @@ export default function Board() {
       : 
       <div>Loading</div>
     }
+      </div>
     </div>
     
   )
