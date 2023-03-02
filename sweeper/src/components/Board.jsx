75404d4791056176a62bd0b74e489549871a08c7
Vladimir Solo <freeman.wushu@mail.ru>
2023-03-02 13:57:36 +0600

    create layout board

--- sweeper/src/components/Board.jsx
+++ sweeper/src/components/Board.jsx
@@ -1,7 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import createBoard from '../lib/createBoard';
 
 export default function Board() {
+  const [grid, setGrid] = useState([]);
+
+  useEffect(() => {
+    const newBoard = () => {
+      const boardLoad =  createBoard(16, 16, 32);
+      setGrid(boardLoad)
+    }
+    newBoard()
+  }, [])
+
   return (
-    <div>Board</div>
+    <div>
+      {grid.board ? 
+        grid.board.map((el) => (
+        <div style={{display: 'flex'}}>
+          {el.map((item) => (
+            <div style={{width: 30, height: 30, border: '1px solid gray'}}>{item.value}</div>
+          ) )}
+          </div>
+      ))
+      : 
+      <div>Loading</div>
+    }
+    </div>
+    
   )
 }
